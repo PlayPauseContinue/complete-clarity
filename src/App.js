6da1f986f09a45cec819ab38116a76b2051e5cb1
PlayPauseContinue <playpausecontinue@gmail.com>
2025-09-16 19:59:31 +0530

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './screens/Home';
@@ -9,24 +10,27 @@ import TextToSpeech from './screens/TextToSpeech';
 import UnitConverter from './screens/UnitConverter';
 function App() {
   return (
-    <Router>
-      <Header />
-      <main
-        style={{
-          paddingTop: '60px',
-          paddingBottom: '50px',
-          minHeight: 'calc(100vh - 110px)',
-        }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/generate-qr" element={<QR />} />
-          <Route path="/image-compressor" element={<ImageCompressor />} />
-          <Route path="/text-to-speech" element={<TextToSpeech />} />
-          <Route path="/unit-converter" element={<UnitConverter />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <main
+          style={{
+            paddingTop: '60px',
+            paddingBottom: '50px',
+            minHeight: 'calc(100vh - 110px)',
+          }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/generate-qr" element={<QR />} />
+            <Route path="/image-compressor" element={<ImageCompressor />} />
+            <Route path="/text-to-speech" element={<TextToSpeech />} />
+            <Route path="/unit-converter" element={<UnitConverter />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
